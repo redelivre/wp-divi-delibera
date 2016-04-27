27e23c33f2ee759e38cb7a1a299ae18c7c4da711
Jacson Passold <jacson@ethymos.com.br>
2016-04-27 00:25:13 -0300

    fix divs format for DIVI

--- single-pauta.php
+++ single-pauta.php
@@ -1,18 +1,34 @@
-<?php get_header(); ?>
+<?php
+get_header();
 
-		<div id="container">
-			<div id="main-content" role="main">
-                <div id="left-area">
+$show_default_title = get_post_meta(get_the_ID(), '_et_pb_show_title', true);
+
+$is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
+
+?>
+
+<div id="main-content">
+	<div class="container">
+		<div id="content-area" class="clearfix">
+			<div id="left-area">
 				<?php
 				// Chama o cabeçalho que apresenta o sistema de discussão
 				get_delibera_header();
-
+				
 				// Chama o loop
-				//get_template_part( 'loop', 'pauta' );
-				load_template(dirname(__FILE__).DIRECTORY_SEPARATOR.'loop-pauta.php', true);
+				// get_template_part( 'loop', 'pauta' );
+				load_template(
+						dirname(__FILE__) . DIRECTORY_SEPARATOR .
+								 'loop-pauta.php', true);
 				?>
-                    </div>
-                <?php get_sidebar(); ?>
-			</div><!-- #content -->
-		</div><!-- #container -->
+             </div>
+			<!-- #left-area -->
+             <?php get_sidebar(); ?>
+		</div>
+		<!-- #content-area -->
+	</div>
+	<!-- .container -->
+</div>
+<!-- #main-content -->
+
 <?php get_footer(); ?>
