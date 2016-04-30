d85f6db5173a418343cba83c39e8b9a0fa8d53bf
Jacson Passold <jacson@ethymos.com.br>
2016-04-30 05:29:04 -0300

    fix entrar on menu on mobile

--- js/ET_Builder_Module_Ajax_Login.js
+++ js/ET_Builder_Module_Ajax_Login.js
@@ -7,8 +7,18 @@ jQuery(document).ready(function() {
 	        seen = true;
 	});
 	//jQuery('.et_pb_ajax_login li.wp-opauth-login-strategy').addClass('et_pb_button');
-	jQuery('.et_pb_ajax_login_button').click(function(event){
-		jQuery('.et_pb_ajax_login').find(".et_pb_ajax_login_panel").toggle();
+	jQuery('.et_pb_ajax_login_button a').attr("href", "javascript:ajax_login_toggle_panel();");
+	jQuery('.et_pb_ajax_login_button a').click(function(event){
 		event.preventDefault();
+		jQuery('.et_pb_ajax_login').find(".et_pb_ajax_login_panel").toggle();
+		
 	});
 });
+
+function ajax_login_toggle_panel()
+{
+	jQuery('.et_pb_ajax_login').find(".et_pb_ajax_login_panel").toggle();
+	jQuery('.et_mobile_menu').toggle();
+	jQuery('.mobile_nav').toggleClass('opened');
+	jQuery('.mobile_nav').toggleClass('closed');
+}
