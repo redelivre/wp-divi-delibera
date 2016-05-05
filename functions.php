10707a03a4e2994a5db2db8abd2ef6bde708e548
Jacson Passold <jacson@ethymos.com.br>
2016-05-05 02:11:53 -0300

    better error report

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@ if(defined(WP_DEBUG) && WP_DEBUG)
 {
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	error_reporting(E_ALL & ~E_STRICT);
+	
 }
 /**
  * Enqueues child theme stylesheet, loading first the parent theme stylesheet.
