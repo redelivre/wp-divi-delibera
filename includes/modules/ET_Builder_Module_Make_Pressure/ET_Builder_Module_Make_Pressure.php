c06b6be84d4b8e6645c16d3a7c44d631ca5252ca
Maurilio Atila <cabelotaina@gmail.com>
2016-09-04 22:32:21 -0300

    added new module button super pressure, new icons, and update public agent page

--- includes/modules/ET_Builder_Module_Make_Pressure/ET_Builder_Module_Make_Pressure.php
+++ includes/modules/ET_Builder_Module_Make_Pressure/ET_Builder_Module_Make_Pressure.php
@@ -329,15 +329,15 @@ class ET_Builder_Module_Make_Pressure extends ET_Builder_Module {
 				<?php endif; ?>
 
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_email', true) ) : ?>
-					<a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" href="mailto:<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>?subject=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,%20...">Email</a>
+					<a style="margin:10px" href="mailto:<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>?subject=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,%20..."><span style="font-size:38px" class="dashicons dashicons-email"></span></a>
 				<?php endif; ?>
 
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_twitter', true) ) : ?>
-				  <a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" href="https://twitter.com/intent/tweet?text=@<?php echo get_post_meta(  get_the_ID(), 'public_agent_twitter', true ); ?>%20por%20favor%20defenda%20a%20exist%C3%AAncia%20do%20MCTI,%20fundamental%20para%20o%20desenvolvimento%20do%20Brasil%20&url=http%3A//goo.gl/Sc9aen&hashtags=FicaMCTI" class="twitter-mention-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				  <a style="margin:10px" class="" href="https://twitter.com/intent/tweet?text=@<?php echo get_post_meta(  get_the_ID(), 'public_agent_twitter', true ); ?>%20por%20favor%20defenda%20a%20exist%C3%AAncia%20do%20MCTI,%20fundamental%20para%20o%20desenvolvimento%20do%20Brasil%20&url=http%3A//goo.gl/Sc9aen&hashtags=FicaMCTI" class="twitter-mention-button" data-show-count="false"><span style="font-size:35px" class="dashicons dashicons-twitter"></span></a>
 				<?php endif; ?>
 				
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_facebook', true) ) : ?>
-				  <a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" target="_brank" href="<?php echo get_post_meta(  get_the_ID(), 'public_agent_facebook', true); ?>"">Facebook</a>
+				  <a style="margin:10px" target="_brank" href="<?php echo get_post_meta(  get_the_ID(), 'public_agent_facebook', true); ?>""><span style="font-size:35px" class="dashicons dashicons-facebook"></span></a>
 				<?php endif; ?>
 				
 				</div> <!-- .et_pb_portfolio_item -->
@@ -750,15 +750,15 @@ class ET_Builder_Module_Filterable_Make_Pressure extends ET_Builder_Module {
 				<?php endif; ?>
 
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_email', true) ) : ?>
-					<a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" href="mailto:<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>?subject=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,%20...">Email</a>
+					<a style="margin:10px" href="mailto:<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>?subject=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissimo%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,%20..."><span style="font-size:38px" class="dashicons dashicons-email"></span></a>
 				<?php endif; ?>
 
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_twitter', true) ) : ?>
-				  <a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" href="https://twitter.com/intent/tweet?text=@<?php echo get_post_meta(  get_the_ID(), 'public_agent_twitter', true ); ?>%20por%20favor%20defenda%20a%20exist%C3%AAncia%20do%20MCTI,%20fundamental%20para%20o%20desenvolvimento%20do%20Brasil%20&url=http%3A//goo.gl/Sc9aen&hashtags=FicaMCTI" class="twitter-mention-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				  <a style="margin:10px" class="" href="https://twitter.com/intent/tweet?text=@<?php echo get_post_meta(  get_the_ID(), 'public_agent_twitter', true ); ?>%20por%20favor%20defenda%20a%20exist%C3%AAncia%20do%20MCTI,%20fundamental%20para%20o%20desenvolvimento%20do%20Brasil%20&url=http%3A//goo.gl/Sc9aen&hashtags=FicaMCTI" class="twitter-mention-button" data-show-count="false"><span style="font-size:35px" class="dashicons dashicons-twitter"></span></a>
 				<?php endif; ?>
-
+				
 				<?php if ( get_post_meta(  get_the_ID(), 'public_agent_facebook', true) ) : ?>
-				  <a class="et_pb_button  et_pb_button_1 et_pb_module et_pb_bg_layout_light" target="_brank" href="<?php echo get_post_meta(  get_the_ID(), 'public_agent_facebook', true); ?>"">Facebook</a>
+				  <a style="margin:10px" target="_brank" href="<?php echo get_post_meta(  get_the_ID(), 'public_agent_facebook', true); ?>""><span style="font-size:35px" class="dashicons dashicons-facebook"></span></a>
 				<?php endif; ?>
 
 				</div><!-- .et_pb_portfolio_item -->
@@ -1191,6 +1191,196 @@ class ET_Builder_Module_Fullwidth_Make_Pressure extends ET_Builder_Module {
 	}
 }
 new ET_Builder_Module_Fullwidth_Make_Pressure;
+
+class ET_Builder_Module_Make_Pressure_Button extends ET_Builder_Module {
+	function init() {
+		$this->name = esc_html__( 'Super Pressão', 'et_builder' );
+		$this->slug = 'et_pb_makepressure_button';
+
+		$this->whitelisted_fields = array(
+			'url_new_window',
+			'include_categories',
+			'button_text',
+			'background_layout',
+			'button_alignment',
+			'admin_label',
+			'module_id',
+			'module_class',
+		);
+
+		$this->fields_defaults = array(
+			'url_new_window'    => array( 'off' ),
+			'background_color'  => array( et_builder_accent_color(), 'add_default_setting' ),
+			'background_layout' => array( 'light' ),
+		);
+
+		$this->main_css_element = '%%order_class%%';
+		$this->advanced_options = array(
+			'button' => array(
+				'button' => array(
+					'label' => esc_html__( 'Button', 'et_builder' ),
+					'css' => array(
+						'main' => $this->main_css_element,
+					),
+				),
+			),
+		);
+	}
+
+	function get_fields() {
+		$fields = array(
+			'include_categories' => array(
+				'label'            => esc_html__( 'Include Categories', 'et_builder' ),
+				'renderer'         => 'et_builder_include_general_categories_option',
+				'option_category'  => 'basic_option',
+				'description'      => esc_html__( 'Select the categories that you would like to include in the feed.', 'et_builder' ),
+			),
+			'url_new_window' => array(
+				'label'           => esc_html__( 'Url Opens', 'et_builder' ),
+				'type'            => 'select',
+				'option_category' => 'configuration',
+				'options'         => array(
+					'off' => esc_html__( 'In The Same Window', 'et_builder' ),
+					'on'  => esc_html__( 'In The New Tab', 'et_builder' ),
+				),
+				'description'       => esc_html__( 'Here you can choose whether or not your link opens in a new window', 'et_builder' ),
+			),
+			'button_text' => array(
+				'label'           => esc_html__( 'Button Text', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Input your desired button text.', 'et_builder' ),
+			),
+			'button_alignment' => array(
+				'label'           => esc_html__( 'Button alignment', 'et_builder' ),
+				'type'            => 'select',
+				'option_category' => 'configuration',
+				'options'         => array(
+					'left'   => esc_html__( 'Left', 'et_builder' ),
+					'center' => esc_html__( 'Center', 'et_builder' ),
+					'right'  => esc_html__( 'Right', 'et_builder' ),
+				),
+				'description'     => esc_html__( 'Here you can define the alignemnt of Button', 'et_builder' ),
+			),
+			'background_layout' => array(
+				'label'           => esc_html__( 'Text Color', 'et_builder' ),
+				'type'            => 'select',
+				'option_category' => 'color_option',
+				'options'         => array(
+					'light' => esc_html__( 'Dark', 'et_builder' ),
+					'dark'  => esc_html__( 'Light', 'et_builder' ),
+				),
+				'description'     => esc_html__( 'Here you can choose whether your text should be light or dark. If you are working with a dark background, then your text should be light. If your background is light, then your text should be set to dark.', 'et_builder' ),
+			),
+			'disabled_on' => array(
+				'label'           => esc_html__( 'Disable on', 'et_builder' ),
+				'type'            => 'multiple_checkboxes',
+				'options'         => array(
+					'phone'   => esc_html__( 'Phone', 'et_builder' ),
+					'tablet'  => esc_html__( 'Tablet', 'et_builder' ),
+					'desktop' => esc_html__( 'Desktop', 'et_builder' ),
+				),
+				'additional_att'  => 'disable_on',
+				'option_category' => 'configuration',
+				'description'     => esc_html__( 'This will disable the module on selected devices', 'et_builder' ),
+			),
+			'admin_label' => array(
+				'label'       => esc_html__( 'Admin Label', 'et_builder' ),
+				'type'        => 'text',
+				'description' => esc_html__( 'This will change the label of the module in the builder for easy identification.', 'et_builder' ),
+			),
+			'module_id' => array(
+				'label'           => esc_html__( 'CSS ID', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'configuration',
+				'tab_slug'        => 'custom_css',
+				'option_class'    => 'et_pb_custom_css_regular',
+			),
+			'module_class' => array(
+				'label'           => esc_html__( 'CSS Class', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'configuration',
+				'tab_slug'        => 'custom_css',
+				'option_class'    => 'et_pb_custom_css_regular',
+			),
+		);
+		return $fields;
+	}
+
+	function shortcode_callback( $atts, $content = null, $function_name ) {
+		$module_id         = $this->shortcode_atts['module_id'];
+		$module_class      = $this->shortcode_atts['module_class'];
+		$button_text       = $this->shortcode_atts['button_text'];
+		$background_layout = $this->shortcode_atts['background_layout'];
+		$url_new_window    = $this->shortcode_atts['url_new_window'];
+		$custom_icon       = $this->shortcode_atts['button_icon'];
+		$button_custom     = $this->shortcode_atts['custom_button'];
+		$button_alignment  = $this->shortcode_atts['button_alignment'];
+		$include_categories = $this->shortcode_atts['include_categories'];
+
+        $args = array(
+			'post_type'      => 'public_agent',
+		);
+
+		if ( '' !== $include_categories )
+			$args['tax_query'] = array(
+				array(
+					'taxonomy' => 'category',
+					'field' => 'id',
+					'terms' => explode( ',', $include_categories ),
+					'operator' => 'IN',
+				)
+			);
+		
+		$the_query = new WP_Query( $args );
+
+		// The Loop
+		$aux = "";
+		$aux2 ="";
+		if ( $the_query->have_posts() ) {
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$aux = get_post_meta(  get_the_ID(), 'public_agent_email', true) ? get_post_meta(  get_the_ID(), 'public_agent_email', true):"";
+				if ($aux) $aux2 .= $aux2 ? "," . $aux: $aux;
+			}
+
+			$button_url = "mailto:" . $aux2 ;
+			/* Restore original Post Data */
+			wp_reset_postdata();
+		} else {
+			// no posts found
+		}
+
+		// Nothing to output if neither Button Text nor Button URL defined
+		if ( '' === $button_text && '' === $button_url ) {
+			return;
+		}
+
+		$module_class = ET_Builder_Element::add_module_order_class( $module_class, $function_name );
+
+		$module_class .= " et_pb_module et_pb_bg_layout_{$background_layout}";
+
+		$output = sprintf(
+			'<div class="et_pb_button_module_wrapper et_pb_module%8$s">
+				<a class="et_pb_button%5$s%7$s" href="%1$s"%3$s%4$s%6$s>%2$s</a>
+			</div>',
+			esc_url( $button_url ),
+			'' !== $button_text ? esc_html( $button_text ) : esc_url( $button_url ),
+			( 'on' === $url_new_window ? ' target="_blank"' : '' ),
+			'' !== $custom_icon && 'on' === $button_custom ? sprintf(
+				' data-icon="%1$s"',
+				esc_attr( et_pb_process_font_icon( $custom_icon ) )
+			) : '',
+			'' !== $custom_icon && 'on' === $button_custom ? ' et_pb_custom_button_icon' : '',
+			( '' !== $module_id ? sprintf( ' id="%1$s"', esc_attr( $module_id ) ) : '' ),
+			( '' !== $module_class ? sprintf( ' %1$s', esc_attr( $module_class ) ) : '' ),
+			'right' === $button_alignment || 'center' === $button_alignment ? sprintf( ' et_pb_button_alignment_%1$s', esc_attr( $button_alignment ) )  : ''
+		);
+
+		return $output;
+	}
+}
+new ET_Builder_Module_Make_Pressure_Button;
 
 if ( ! function_exists( 'et_builder_include_general_categories_option' ) ) :
 function et_builder_include_general_categories_option( $args = array() ) {
