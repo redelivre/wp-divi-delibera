c534bf591a3a206cd6dd624a2461d34d5523e4ad
Maurilio Atila <cabelotaina@gmail.com>
2016-09-20 11:35:41 -0300

    new search form for make pressure

--- includes/modules/ET_Builder_Module_Make_Pressure/ET_Builder_Module_Make_Pressure.php
+++ includes/modules/ET_Builder_Module_Make_Pressure/ET_Builder_Module_Make_Pressure.php
@@ -2493,6 +2493,204 @@ class ET_Builder_Module_Statistics extends ET_Builder_Module {
 }
 new ET_Builder_Module_Statistics;
 
+class ET_Builder_Module_Make_Pressure_Search extends ET_Builder_Module {
+	function init() {
+		$this->name = esc_html__( 'Busca - BP', 'et_builder' );
+		$this->slug = 'et_pb_search_bp';
+
+		$this->whitelisted_fields = array(
+			'background_layout',
+			'text_orientation',
+			'content_new',
+			'admin_label',
+			'module_id',
+			'module_class',
+			'max_width',
+			'max_width_tablet',
+			'max_width_phone',
+		);
+
+		$this->fields_defaults = array(
+			'background_layout' => array( 'light' ),
+			'text_orientation'  => array( 'left' ),
+		);
+
+		$this->main_css_element = '%%order_class%%';
+		$this->advanced_options = array(
+			'fonts' => array(
+				'text'   => array(
+					'label'    => esc_html__( 'Text', 'et_builder' ),
+					'css'      => array(
+						'line_height' => "{$this->main_css_element} p",
+					),
+				),
+			),
+			'background' => array(
+				'settings' => array(
+					'color' => 'alpha',
+				),
+			),
+			'border' => array(),
+			'custom_margin_padding' => array(
+				'css' => array(
+					'important' => 'all',
+				),
+			),
+		);
+	}
+
+	function get_fields() {
+		$fields = array(
+			'background_layout' => array(
+				'label'             => esc_html__( 'Text Color', 'et_builder' ),
+				'type'              => 'select',
+				'option_category'   => 'configuration',
+				'options'           => array(
+					'light' => esc_html__( 'Dark', 'et_builder' ),
+					'dark'  => esc_html__( 'Light', 'et_builder' ),
+				),
+				'description'       => esc_html__( 'Here you can choose the value of your text. If you are working with a dark background, then your text should be set to light. If you are working with a light background, then your text should be dark.', 'et_builder' ),
+			),
+			'text_orientation' => array(
+				'label'             => esc_html__( 'Text Orientation', 'et_builder' ),
+				'type'              => 'select',
+				'option_category'   => 'layout',
+				'options'           => et_builder_get_text_orientation_options(),
+				'description'       => esc_html__( 'This controls the how your text is aligned within the module.', 'et_builder' ),
+			),
+			'content_new' => array(
+				'label'           => esc_html__( 'Content', 'et_builder' ),
+				'type'            => 'tiny_mce',
+				'option_category' => 'basic_option',
+				'description'     => esc_html__( 'Here you can create the content that will be used within the module.', 'et_builder' ),
+			),
+			'max_width' => array(
+				'label'           => esc_html__( 'Max Width', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'layout',
+				'mobile_options'  => true,
+				'tab_slug'        => 'advanced',
+				'validate_unit'   => true,
+			),
+			'max_width_tablet' => array(
+				'type' => 'skip',
+			),
+			'max_width_phone' => array(
+				'type' => 'skip',
+			),
+			'disabled_on' => array(
+				'label'           => esc_html__( 'Disable on', 'et_builder' ),
+				'type'            => 'multiple_checkboxes',
+				'options'         => array(
+					'phone'   => esc_html__( 'Phone', 'et_builder' ),
+					'tablet'  => esc_html__( 'Tablet', 'et_builder' ),
+					'desktop' => esc_html__( 'Desktop', 'et_builder' ),
+				),
+				'additional_att'  => 'disable_on',
+				'option_category' => 'configuration',
+				'description'     => esc_html__( 'This will disable the module on selected devices', 'et_builder' ),
+			),
+			'admin_label' => array(
+				'label'       => esc_html__( 'Admin Label', 'et_builder' ),
+				'type'        => 'text',
+				'description' => esc_html__( 'This will change the label of the module in the builder for easy identification.', 'et_builder' ),
+			),
+			'module_id' => array(
+				'label'           => esc_html__( 'CSS ID', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'configuration',
+				'tab_slug'        => 'custom_css',
+				'option_class'    => 'et_pb_custom_css_regular',
+			),
+			'module_class' => array(
+				'label'           => esc_html__( 'CSS Class', 'et_builder' ),
+				'type'            => 'text',
+				'option_category' => 'configuration',
+				'tab_slug'        => 'custom_css',
+				'option_class'    => 'et_pb_custom_css_regular',
+			),
+		);
+		return $fields;
+	}
+
+	function shortcode_callback( $atts, $content = null, $function_name ) {
+		$module_id            = $this->shortcode_atts['module_id'];
+		$module_class         = $this->shortcode_atts['module_class'];
+		$background_layout    = $this->shortcode_atts['background_layout'];
+		$text_orientation     = $this->shortcode_atts['text_orientation'];
+		$max_width            = $this->shortcode_atts['max_width'];
+		$max_width_tablet     = $this->shortcode_atts['max_width_tablet'];
+		$max_width_phone      = $this->shortcode_atts['max_width_phone'];
+
+		$module_class = ET_Builder_Element::add_module_order_class( $module_class, $function_name );
+
+		$this->shortcode_content = et_builder_replace_code_content_entities( $this->shortcode_content );
+
+		if ( '' !== $max_width_tablet || '' !== $max_width_phone || '' !== $max_width ) {
+			$max_width_values = array(
+				'desktop' => $max_width,
+				'tablet'  => $max_width_tablet,
+				'phone'   => $max_width_phone,
+			);
+
+			et_pb_generate_responsive_css( $max_width_values, '%%order_class%%', 'max-width', $function_name );
+		}
+
+		if ( is_rtl() && 'left' === $text_orientation ) {
+			$text_orientation = 'right';
+		}
+
+		$class = " et_pb_module et_pb_bg_layout_{$background_layout} et_pb_text_align_{$text_orientation}";
+
+		$output = sprintf(
+			'<div%3$s class="et_pb_text%2$s%4$s">
+				%1$s
+			</div> <!-- .et_pb_text -->',
+			$this->shortcode_content,
+			esc_attr( $class ),
+			( '' !== $module_id ? sprintf( ' id="%1$s"', esc_attr( $module_id ) ) : '' ),
+			( '' !== $module_class ? sprintf( ' %1$s', esc_attr( $module_class ) ) : '' )
+		);
+
+		$output .= '<form method="get" action="' . get_site_url() . '">';
+		 	$output .= '<p>';
+			$output .= '<input type="text" name="s">';
+			$output .= '<button>Buscar</button>';
+			$output .= '</p>';
+
+			$args = array( 'taxonomy' => 'public_agent_party','name' => 'public_agent_party', 'show_count' => 1, 'value_field' => 'slug', 'echo' => 0, 'show_option_none' => 'Selecione', 'option_none_value' => '');
+			$output .= '<p> Partido: ';
+			$output .= wp_dropdown_categories( $args );
+			$output .= '</p>';
+
+			$args['taxonomy'] = 'public_agent_state';
+			$args['name'] = 'public_agent_state';
+			
+			$output .= '<p>Estado: ';
+	 		$output .= wp_dropdown_categories( $args );
+	 		$output .= '</p>';
+
+	 		$args['taxonomy'] = 'public_agent_genre';
+	 		$args['name'] = 'public_agent_genre';
+	 		
+	 		$output .= '<p>Genêro: ';
+	 		$output .= wp_dropdown_categories( $args );
+	 		$output .= '</p>';
+
+	 		$args['taxonomy'] = 'public_agent_job';
+	 		$args['name'] = 'public_agent_job';
+	 		
+	 		$output .= '<p>Cargo: ';
+	 		$output .= wp_dropdown_categories( $args );
+	 		$output .= '</p>';
+
+		$output .= '</form>';
+
+		return $output;
+	}
+}
+new ET_Builder_Module_Make_Pressure_Search;
+
 // functions
 
 if ( ! function_exists( 'et_builder_include_general_categories_option' ) ) :
