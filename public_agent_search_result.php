c3284d2caff1acac55c3ad6b4fc9d0093205f289
Maurilio Atila <cabelotaina@gmail.com>
2016-09-28 12:01:32 -0300

    added new makepressure button on search results

--- public_agent_search_result.php
+++ public_agent_search_result.php
@@ -12,6 +12,39 @@ if ( have_posts() ) {
 <div class="entry-content">
   <div class="et_pb_section  et_pb_section_0 et_section_regular">
     <div class=" et_pb_row et_pb_row_0">
+      <?php
+        $button_url = "mailto:";
+        $button_text = "Super Pressão!";
+        $emails = "";
+        $aux = "";
+        // The Loop
+        if ( have_posts() ) {
+
+          while ( have_posts() ) {
+            the_post();
+            $emails = get_post_meta(  get_the_ID(), 'public_agent_email', true) ? get_post_meta(  get_the_ID(), 'public_agent_email', true):"";
+            if ($emails) $aux .= $aux ? "," . $emails: $emails;
+          }
+          wp_reset_postdata();
+          /* Restore original Post Data */
+        } else {
+          // no posts found
+        }
+        $button_url .= $aux . "?subject=" . get_option('makepressure_email_title') . "&body=" . get_option('makepressure_email_body');
+        // Nothing to output if neither Button Text nor Button URL defined
+        if ( '' === $button_text && '' === $button_url ) {
+          return;
+        }
+        $module_class = " et_pb_module";
+        $output = sprintf(
+          '<div class="et_pb_button_module_wrapper makepressure_search_button">
+            <a class="makepressure_superpressure et_pb_button  et_pb_makepressure_button_0 et_pb_module et_pb_bg_layout_light" href="%1$s">%2$s</a>
+          </div>',
+          esc_url( $button_url ),
+          '' !== $button_text ? esc_html( $button_text ) : esc_url( $button_url )
+        );
+          echo $output;
+      ?>
       <h1><?php _e("Veja o resultado da sua Busca", "makepressure"); ?>:</h1>
       <div class="et_pb_column et_pb_column_4_4  et_pb_column_2">
         <div class="et_pb_portfolio_grid clearfix et_pb_module et_pb_bg_layout_light  et_pb_public_agent_0">
