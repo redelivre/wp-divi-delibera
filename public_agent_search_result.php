b2c2de05ace336e416ee6a89deacfc3249d169fe
Maurilio Atila <cabelotaina@gmail.com>
2016-10-17 12:17:46 -0200

    using the philosophy dont repeat youself

--- public_agent_search_result.php
+++ public_agent_search_result.php
@@ -95,47 +95,27 @@ if ( have_posts() ) {
               ?>
               </strong>
             </div>
-            <a href="<?php esc_url( the_permalink() ); ?>">
-              <?php if(has_post_thumbnail()) : ?>
-              <?php the_post_thumbnail(array(175,175), array('class' => 'makepressure_post_main_image')); ?>
-              <?php endif; ?>
-            </a>
-
-            <p class="post-meta"></p>
             <?php 
-              $email_subject = get_option( 'makepressure_email_title' );
-              $email_body = get_option( 'makepressure_email_body' );
-              $more_emails = get_option( 'makepressure_more_emails' );
-
-              $twitter_text = get_option( 'makepressure_twitter_text' );
-              $twitter_url = get_option( 'makepressure_twitter_url' );
-              $twitter_hashtag = get_option( 'makepressure_twitter_hashtag' );
+            $cargo = wp_get_post_terms( get_the_ID() , 'public_agent_job' ) ? wp_get_post_terms(  get_the_ID(), 'public_agent_job', true) : '';
+            $cargo = isset($cargo[0]) ? $cargo[0] : '';
+            if($cargo):
             ?>
-
-              <div class="makepressure_action" >
-                <?php
-                $genre = wp_get_post_terms( get_the_ID() , 'public_agent_genre');
-                if (is_array($genre)) {
-                    $genre = $genre[0];
-                    $genre_slug = $genre->slug;
-                }
-                if ( get_post_meta(  get_the_ID(), 'public_agent_email', true) ) : ?>
-                  <a id="<?php echo get_the_ID(); ?>" class="fa fa-3x fa-envelope makepressure_email" href="mailto:<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>?subject=Excelentissim<?php echo $genre_slug=='feminino'?'a':'o'; ?>%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissim<?php echo $genre_slug=='feminino'?'a':'o'; ?>%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,  %0A%0A<?php echo $email_body; ?>" ></a>
+              <a href="<?php esc_url( the_permalink() ); ?>">
+                <?php if(has_post_thumbnail()) : ?>
+                  <?php the_post_thumbnail(array(175,175), array('class' => 'makepressure_' . $cargo->slug . ' makepressure_post_main_image')); ?>
                 <?php endif; ?>
-
-                <?php if ( get_post_meta(  get_the_ID(), 'public_agent_email', true) ) : ?>
-                  <a id="<?php echo get_the_ID(); ?>" target="_blank" class="fa fa-3x fa-google makepressure_gmail" href="https://mail.google.com/mail?view=cm&tf=0&to=<?php print_r(get_post_meta(  get_the_ID(), 'public_agent_email', true)); ?>&su=Excelentissim<?php echo $genre_slug=='feminino'?'a':'o'; ?>%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true)?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>&body=Excelentissim<?php echo $genre_slug=='feminino'?'a':'o'; ?>%20<?php echo get_post_meta(  get_the_ID(), 'public_agent_cargo', true) ?get_post_meta(  get_the_ID(), 'public_agent_cargo', true):""; ?>%20<?php echo get_the_title(); ?>,  %0A%0A<?php echo $email_body; ?>" ></a>
-                <?php endif; ?>
-
-                <?php if ( get_post_meta(  get_the_ID(), 'public_agent_twitter', true) ) : ?>
-                  <a id="<?php echo get_the_ID(); ?>" class="fa fa-twitter fa-3x makepressure_twitter" href="https://twitter.com/intent/tweet?text=@<?php echo get_post_meta(  get_the_ID(), 'public_agent_twitter', true ); ?><?php echo $twitter_text; ?>&url=<?php echo $twitter_url; ?>&hashtags=<?php echo $twitter_hashtag; ?>" data-show-count="false"></a>
-                <?php endif; ?>
-                
-                <?php if ( get_post_meta(  get_the_ID(), 'public_agent_facebook', true) ) : ?>
-                  <a id="<?php echo get_the_ID(); ?>" class="fa fa-facebook-official fa-3x makepressure_facebook" target="_brank" href="<?php echo get_post_meta(  get_the_ID(), 'public_agent_facebook', true); ?>"></a>
-                <?php endif; ?>
-                </div>
-              </div> <!-- .et_pb_portfolio_item -->
+              </a>
+            <?php
+            endif; 
+            $email_subject = get_option( 'makepressure_email_title' );
+            $email_body = get_option( 'makepressure_email_body' );
+            $more_emails = get_option( 'makepressure_more_emails' );
+            $twitter_text = get_option( 'makepressure_twitter_text' );
+            $twitter_url = get_option( 'makepressure_twitter_url' );
+            $twitter_hashtag = get_option( 'makepressure_twitter_hashtag' );
+            ?>
+            <?php wp_divi_get_share_buttons(); ?>
+            </div> <!-- .et_pb_portfolio_item -->
           <?php
           }
           ?>
